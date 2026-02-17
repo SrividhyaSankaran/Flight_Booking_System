ba7053a36f12f2fac4df40dd830e96409443b479
Srividhya Sankaran <ssrividhyasankaran@gmail.com>
2026-02-17 23:52:27 +0530

    Initial commit: Auth Service project setup

---
+++ auth-service/src/main/java/com/lms/auth_service/dto/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.lms.auth_service.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LoginRequest {
+    private String username;
+    private String password;
+}
